2af5bee89fa8adddbe366b7cc54ea24927eb42bd
rajesh <krajeshgowri7032@mail.com>
2022-08-16 17:35:45 +0530

    Javascript-function

---
+++ display.js
@@ -0,0 +1,5 @@
+function name (){
+    console.log("hello world");
+    
+}
+name();
---
+++ function expresion.js
@@ -0,0 +1,8 @@
+let x= function (num) {
+    return (num*num);
+}
+console.log(x(5));
+
+
+let z= 5;
+console.log(x(z));
